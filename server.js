e9285fac94d3a9e1bf8e5c0e176f223c52935465
HafizHamza500 <psteamwebdeveloper1@gmail.com>
2026-06-22 21:44:35 +0500

    port change

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.post("/api/lead", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
